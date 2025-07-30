0b7ba981b4da02844c84b3ade2e9644a8478df60
TraMiya <tramiyamcneese@gmail.com>
2025-07-29 23:35:35 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,19 +1,17 @@
-("Enter your first number: 10 ")
+num1 = float(input("Enter first number:" ))
+num2 = float(input("Enter second number: "))
+op = input("Enter operation (+, -, *, /): ")
 
-("Enter your second number: 5 ")
-
-("What do you want to do? (+, -, *, /):")
-
-if op =="+":
-    print("answer is:", num1 + num2)
-elif op == "-:"
-    print("answer is:", num1 - num2)
+if op == "+":
+    print=( num1 + num2)
+elif op == "-":
+    print( num1 - num2)
 elif op == "*":
-    print("answer is:", num1 * num2)
+    print( num1 * num2)
 elif op == "/":
-    if num2 ==0:
+    if num2 == 0:
         print("cant divide by 0")
     else:
-        print(answer is:, num1 / num2)
+        print("answer is:", num1 / num2)
 else:
     print("not a true operation")
